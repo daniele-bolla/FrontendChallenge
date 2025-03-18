ecf4221bf4ba4e752b4d130dab19515580830d2b
bolladaniele <bolladaniele@gmail.com>
2025-03-18 13:18:58 +0000

    add fittext and material inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router'
+import { Ng2FittextModule } from "ng2-fittext";
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Ng2FittextModule, FormsModule, MatInputModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+
 })
 export class AppComponent {
-  title = 'FrontendChallenge'
+  title = 'Time to Midsummer Eve'
+  countdown = '20 days, 3 h, 15m, 10s'
+  date = '2024-06-21'
+  onSubmit(){
+    console.log(2134)
+  }
+
 }
+
+
