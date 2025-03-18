3e9dc4c0738f2b9d20a23850b521b86d63021fd0
bolladaniele <bolladaniele@gmail.com>
2025-03-18 14:13:44 +0000

    add countdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,15 @@ import { RouterOutlet } from '@angular/router'
 import { Ng2FittextModule } from "ng2-fittext";
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+import utc from 'dayjs/plugin/utc';
+import { Subscription, interval } from 'rxjs';
+import timezone from 'dayjs/plugin/timezone';
+dayjs.extend(duration);
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.tz.setDefault()
 
 @Component({
   selector: 'app-root',
@@ -13,13 +22,49 @@ import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 })
 export class AppComponent {
+
   title = 'Time to Midsummer Eve'
-  countdown = '20 days, 3 h, 15m, 10s'
-  date = '2024-06-21'
-  onSubmit(){
-    console.log(2134)
+  date = '2025-06-21'
+  countdown: { days: number; hours: number; minutes: number; seconds: number } = { days: 0, hours: 0, minutes: 0, seconds: 0 };
+  private subscription: Subscription | undefined;
+
+  ngOnInit(): void {
+    this.startCountdown();
   }
 
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  startCountdown(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    this.subscription = interval(1000).subscribe(() => {
+      this.countdown = this.calculateCountdown(this.date);
+    });
+  }
+
+  private calculateCountdown(endDate: string): { days: number; hours: number; minutes: number; seconds: number } {
+    const now = dayjs();
+    const duration = dayjs.duration(dayjs(endDate).diff(now));
+    return {
+      days: dayjs(endDate).diff(now,'days'),
+      hours: duration.hours(),
+      minutes: duration.minutes(),
+      seconds: duration.seconds(),
+    };
+  }
+
+  formatCountdown(countdown: { days: number; hours: number; minutes: number; seconds: number }): string {
+    return `${countdown.days} days, ${countdown.hours} h, ${countdown.minutes}m, ${countdown.seconds}s`;
+  }
 }
 
+
+function intervalToDuration(arg0: { start: Date; end: Date; }) {
+  throw new Error('Function not implemented.');
+}
 
