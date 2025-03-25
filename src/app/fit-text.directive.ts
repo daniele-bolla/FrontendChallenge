f3f9ee4ac3e9eb3a0797592c794d3c7f3635fc76
bolladaniele <bolladaniele@gmail.com>
2025-03-25 19:41:27 +0000

    add own drective fit text

--- src/app/fit-text.directive.ts
+++ src/app/fit-text.directive.ts
@@ -3,13 +3,18 @@ import {
   ElementRef,
   AfterViewInit,
   HostListener,
+  Input,
+  OnChanges,
+  SimpleChanges,
 } from '@angular/core';
 
 @Directive({
   selector: '[appFitText]',
   standalone: true,
 })
-export class FitTextDirective implements AfterViewInit {
+export class FitTextDirective implements AfterViewInit, OnChanges {
+  @Input() modelToWatch = '';
+
   constructor(private fitTextElement: ElementRef) {
     this.fitTextElement.nativeElement.style.whiteSpace = 'nowrap';
     this.fitTextElement.nativeElement.style.textWrap = 'nowrap';
@@ -17,7 +22,18 @@ export class FitTextDirective implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-    this.fitText(this.fitTextElement.nativeElement);
+    if (this.fitTextElement.nativeElement) {
+      // this.fitText(this.fitTextElement.nativeElement);
+    }
+  }
+
+  public ngOnChanges(changes: SimpleChanges) {
+    if (changes['modelToWatch']) {
+      this.fitTextElement.nativeElement.innerHTML = this.modelToWatch;
+      if (!changes['modelToWatch'].firstChange) {
+        this.fitText(this.fitTextElement.nativeElement);
+      }
+    }
   }
 
   @HostListener('window:resize', ['$event'])
